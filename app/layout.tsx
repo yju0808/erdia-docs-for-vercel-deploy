e1d0f0b2556c59a9453b92d17aabb2e1cb6f065c
Kant Lim <yju0808@naver.com>
2025-10-27 10:56:27 +0000

    example docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { Noto_Sans_KR } from 'next/font/google';
 
-const inter = Inter({
+const notoSansKR = Noto_Sans_KR({
   subsets: ['latin'],
+  weight: ['400', '500', '700'],
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="ko" className={notoSansKR.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
