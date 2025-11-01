85b97c40e97d2713fe2c1e342acebe5f155ca204
Kant Lim <yju0808@naver.com>
2025-11-01 02:06:50 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,26 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import type { Metadata } from 'next';
 import './global.css';
-import { Noto_Sans_KR } from 'next/font/google';
+import localFont from 'next/font/local'
 
-const notoSansKR = Noto_Sans_KR({
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
-});
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://docs.erdia.app'),
+  title: {
+    default: 'Erdia 문서',
+    template: '%s | Erdia 문서'
+  },
+  description: 'Erdia - AI 기반 ERD 다이어그램 툴 공식 문서',
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="ko" className={notoSansKR.className} suppressHydrationWarning>
+    <html lang="ko" className={pretendard.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
